1e898f3ae8b6296523f367147783cfde309a4ce4
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 17:31:33 +0200

    Added the initial startup model

--- startup_gateway/startups/models.py
+++ startup_gateway/startups/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class StartupProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='startup_profile'
+    )
+    company_name = models.CharField(max_length=255)
+    short_pitch = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+
+    class Meta:
+        db_table = 'StartupProfile'
+        indexes = [
+            models.Index(fields=['company_name']),
+        ]
+
+    def __str__(self):
+        return self.company_name
