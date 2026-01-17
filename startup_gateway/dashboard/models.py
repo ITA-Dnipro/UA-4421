dc63d54c95c9917e191e10dbcde2e1292f97332d
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 17:57:29 +0200

    Added the initial savedStartups  model

--- startup_gateway/dashboard/models.py
+++ startup_gateway/dashboard/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db import models 
+from startups.models import StartupProfile 
+from investors.models import InvestorProfile 
 
-# Create your models here.
+
+class SavedStartap(models.Model): 
+    investor_profile = models.ForeignKey(
+        InvestorProfile, 
+        on_delete=models.CASCADE,
+        related_name='saved_startups'
+        ) 
+    startap_profile = models.ForeignKey(
+        StartupProfile, 
+        on_delete=models.CASCADE,
+        related_name='saved_by_investors'
+        ) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta: 
+        db_table = 'SavedStartap'
+        unique_together = ('investor', 'startup')
+
+    def __str__(self):
+        return f'{self.investor_profile} saved {self.startap_profile}'
