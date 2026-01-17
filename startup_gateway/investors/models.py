47c7459bf166efe9ebe20ce10314e0bb8c766dbe
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 17:32:12 +0200

    Added the initial investor model

--- startup_gateway/investors/models.py
+++ startup_gateway/investors/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class InvestorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='investor_profile'
+    )
+    company_name = models.CharField(max_length=255)
+   
+    class Meta:
+        db_table = 'InvestorProfile'
+        indexes = [
+            models.Index(fields=['company_name']),
+        ]
+
+    def __str__(self):
+        return self.company_name
