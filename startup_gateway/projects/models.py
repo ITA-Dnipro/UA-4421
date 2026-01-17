4708894b62f2a9fab21ede7d17ffaa394c96284d
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 17:32:54 +0200

    Added the initial project model

--- startup_gateway/projects/models.py
+++ startup_gateway/projects/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
+from startups.models import StartupProfile
 
-# Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    startup_profile = models.ForeignKey(StartupProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=50, default='idea')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Projects'
+        indexes = [
+            models.Index(fields=['title']),
+        ]
+
+    def __str__(self):
+        return self.title
