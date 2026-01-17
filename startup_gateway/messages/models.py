5e9c19fceef95610ac82b12c54ca4011cb65b330
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 17:33:30 +0200

    Added the initial message model

--- startup_gateway/messages/models.py
+++ startup_gateway/messages/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from projects.models import Project
+from users.models import User
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Messages'
+        indexes = [
+            models.Index(fields=['sender']),
+            models.Index(fields=['receiver']),
+            models.Index(fields=['project']),
+        ]
+
+    def __str__(self):
+        return f'Message from {self.sender} to {self.receiver}'
