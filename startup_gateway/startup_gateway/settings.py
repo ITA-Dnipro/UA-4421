929be729b2ede91b3c364aee3efafbbfdbd310f8
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-17 18:56:33 +0200

    Created and executed migrations

--- startup_gateway/startup_gateway/settings.py
+++ startup_gateway/startup_gateway/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 AUTH_USER_MODEL = 'users.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
