9a1a58838be8711bae248eead09729cca9064217
Rostyk <rostykkaziuta@gmail.com>
2026-01-22 17:19:17 +0200

    done task #32

--- startup_gateway/startup_gateway/settings.py
+++ startup_gateway/startup_gateway/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
 
--- startup_gateway/startup_gateway/urls.py
+++ startup_gateway/startup_gateway/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 def health_check(request):
     return JsonResponse({"status": "ok"})
 
@@ -26,4 +31,9 @@ urlpatterns = [
     path('api/health/', health_check),
     path("api/auth/", include("users.urls")),
     path("", include("startup_gateway.content.urls")),
+
+    path("api/", include("projects.urls")),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
