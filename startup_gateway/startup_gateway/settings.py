2417f491fcf6c345fdfd509ead24b3d043f6263c
Yuliia Klimchuk <yulia.klimchuk95@gmail.com>
2026-01-22 13:30:05 +0200

    Created a docker-compose.yml file for the backend and an env.example file for the database. Added project startup steps to the readme file

--- startup_gateway/startup_gateway/settings.py
+++ startup_gateway/startup_gateway/settings.py
@@ -93,12 +93,26 @@ WSGI_APPLICATION = 'startup_gateway.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_ENGINE = os.getenv("DB_ENGINE", "sqlite")
+
+if DB_ENGINE == "postgres":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
