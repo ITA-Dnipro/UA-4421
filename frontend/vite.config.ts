866dc4ea8d1007985b1cb7290aa387e1ede6369c
PavloNaichuk <pavlo.naichuk@gmail.com>
2026-02-02 18:08:34 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />h
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import { defineConfig as defineVitestConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
