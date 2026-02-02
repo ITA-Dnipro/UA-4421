afa3202c71bb65f07d34259e246b6cfa2dc2ef8c
PavloNaichuk <pavlo.naichuk@gmail.com>
2026-02-02 23:02:42 +0200

    Update RegisterStartup.test.tsx

--- frontend/src/pages/RegisterStartup.test.tsx
+++ frontend/src/pages/RegisterStartup.test.tsx
@@ -42,7 +42,6 @@ describe('RegisterStartup', () => {
     vi.stubGlobal('fetch', fetchMock)
 
     render(<RegisterStartup />)
-
     await user.click(screen.getByRole('button', { name: 'Register' }))
 
     expect(fetchMock).not.toHaveBeenCalled()
