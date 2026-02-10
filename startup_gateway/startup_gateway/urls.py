9472cb352d50bab0aba1fc426a7a2d0000ec4e05
Yana Hrabar <hrabaryan@gmail.com>
2026-02-10 17:18:16 +0200

    fix bug with users migrations

--- startup_gateway/startup_gateway/urls.py
+++ startup_gateway/startup_gateway/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path("api/", include("dashboard.urls")),
     path('api/', include(('startups.api.urls', 'startups'), namespace='startups_api')),
     path("api/", include("projects.urls")),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
-
