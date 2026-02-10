722e28966784fc8e75bb24d35e3dba0fb9198ef6
Rostyk <rostykkaziuta@gmail.com>
2026-02-10 16:53:10 +0200

    hope that i'm fixed control on refresh endpoint

--- startup_gateway/users/urls.py
+++ startup_gateway/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView, ResendVerificationView, PasswordResetRequestView, LoginView, LogoutView
-from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, VerifyEmailView, ResendVerificationView, PasswordResetRequestView, LoginView, LogoutView, OwnTokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="auth-register"),
@@ -9,5 +8,5 @@ urlpatterns = [
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path("login/", LoginView.as_view(), name="auth-login"),
     path('logout/', LogoutView.as_view(), name='auth-logout'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('refresh/', OwnTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- startup_gateway/users/views.py
+++ startup_gateway/users/views.py
@@ -5,8 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.throttling import AnonRateThrottle
+from rest_framework_simplejwt.views import TokenRefreshView
 import logging
-from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer
+from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer, OpenApiExample
 from .serializers import RegisterSerializer, VerifyEmailSerializer, ResendVerificationSerializer, PasswordResetRequestSerializer, LoginSerializer, LogoutSerializer
 from .services import send_verification_email, verify_email_token, is_resend_verification_throttled
 from .tokens import password_reset_token_generator
@@ -204,3 +205,31 @@ class LogoutView(APIView):
         serializers.save()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@extend_schema(
+    tags=["Auth"],
+    description="Refresh access token using refresh token.",
+    responses={
+        200: OpenApiResponse(description="New access token issued"),
+        401: OpenApiResponse(description="Refresh token invalid/expired"),
+    },
+    examples=[
+        OpenApiExample(
+            "Success",
+            value={"access": "jwt...", "refresh": "jwt..."},
+            response_only=True,
+        ),
+        OpenApiExample(
+            "Invalid token",
+            value={"detail": "Token is invalid or expired", "code": "token_not_valid"},
+            status_codes=["401"],
+            response_only=True,
+        ),
+    ],
+)
+
+class OwnTokenRefreshView(TokenRefreshView):
+    """
+    Here we can add throttling classes, permissions, logging, etc.
+    """
+    pass
