82e238d946a8920ad93aa735e771e85f0d4428dd
IhorBohush <boigor95@gmail.com>
2026-02-17 11:50:21 +0200

    Fixed minor changes and updated

--- startup_gateway/startups/views.py
+++ startup_gateway/startups/views.py
@@ -1,4 +1,4 @@
-from django.forms import ValidationError
+from rest_framework.exceptions import ValidationError
 from django.shortcuts import render
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from .models import StartupProfile
